bc7c45d819b8a2ce6b46430fb9a8c6a0b0154826
Alan Jimenez <alanjq@outlook.com>
2024-02-27 10:09:03 -0500

    Add urlencode

--- server/index.js
+++ server/index.js
@@ -3,9 +3,8 @@ const { blogRouter } = require('./router');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.json({ message: `API server is working on port: ${PORT}` })
