bea379c4aa3fea12b4c8212914fdfb5a43c1594f
Alan Jimenez <alanjq@outlook.com>
2024-02-27 09:24:14 -0500

    Methods to get and search

--- server/controllers/BlogController.js
+++ server/controllers/BlogController.js
@@ -1,8 +1,47 @@
 const db = require('../mysql/db')
 
+const TABLE_COLUMNS = ['id', 'title', 'published', 'article', 'author']
+// const TABLE_COLUMNS = [SELECT id, title, published, article, author FROM blog_post WHERE CONCAT(title,article, author) LIKE "%alan%"]
+
 async function getBlogPosts() {
-    const results = await db.query(`SELECT * FROM blog_post`)
+    const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post`)
     return results
 }
 
-module.exports = { getBlogPosts }
+async function searchByTitle(title) {
+    const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE title LIKE "%${title}%"`)
+    return results
+}
+
+async function searchByAuthor(author) {
+    const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE author LIKE "%${author}%"`)
+    return results
+}
+
+async function searchByArticle(article) {
+    const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE article LIKE "%${article}%"`)
+    return results
+}
+
+async function getById(id) {
+    const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE id=${id}`)
+    return results
+}
+
+async function searchAllContent(searchValue) {
+    let myQuery = `SELECT {0} FROM blog_post WHERE CONCAT({1}) LIKE "%${searchValue}%"`
+    myQuery = myQuery.replace('{0}', TABLE_COLUMNS.join(','))
+    myQuery = myQuery.replace('{1}', TABLE_COLUMNS.slice(1, 5).join(`,`))
+    const results = await db.query(myQuery)
+    return results
+}
+
+
+module.exports = {
+    getBlogPosts,
+    searchByTitle,
+    searchByAuthor,
+    searchByArticle,
+    getById,
+    searchAllContent
+}
