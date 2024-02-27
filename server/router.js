6537c80b2676db9222df37d5bfe714d3a0de45f2
Alan Jimenez <alanjq@outlook.com>
2024-02-27 09:24:39 -0500

    Router for app

--- server/router.js
+++ server/router.js
@@ -4,11 +4,24 @@ const blogRouter = express.Router();
 
 blogRouter.get('/', async function (req, res, next) {
     try {
-        res.json(await BlogController.getBlogPosts());
+        if (Object.keys(req.query).length === 0) {
+            res.json({ data: await BlogController.getBlogPosts() });
+        }
+        if (req.query.q) {
+            res.json({ data: await BlogController.searchAllContent(req.query.q) });
+        } else if (req.query.title) {
+            res.json({ data: await BlogController.searchByTitle(req.query.title) });
+        }
+        else if (req.query.author) {
+            res.json({ data: await BlogController.searchByAuthor(req.query.author) });
+        }
+        else if (req.query.article) {
+            res.json({ data: await BlogController.searchByArticle(req.query.article) });
+        }
     } catch (error) {
-        console.error('API Error:', err.message);
+        console.error('API Error:', error.message);
         next(error);
     }
 })
 
-module.exports = {blogRouter}
+module.exports = { blogRouter }
