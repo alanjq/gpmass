e735df250395e8b106675a2dc40e21ae1ad273a0
Alan Jimenez <alanjq@outlook.com>
2024-02-28 16:35:33 -0500

    Style PostList component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,7 +1,7 @@
 import BlogEntry from "./BlogEntry";
 
 export default function PostList({ data, extract }) {
-    return (<section className="flex flex-wrap flex-col gap-2">
+    return (<section className="flex flex-wrap flex-col gap-2 max-w-96">
         {Array.isArray(data) ? data.map((blogentry, key) =>
             <BlogEntry key={key} {...blogentry} extract={extract} />
         ) : null}
