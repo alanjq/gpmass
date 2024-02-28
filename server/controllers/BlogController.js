8fd1ebb6b294ded7b3e242d5e83d3afbaab39235
Alan Jimenez <alanjq@outlook.com>
2024-02-28 13:22:44 -0500

    Update and validate queries

--- server/controllers/BlogController.js
+++ server/controllers/BlogController.js
@@ -4,48 +4,77 @@ const TABLE_COLUMNS = ['id', 'title', 'published', 'article', 'author']
 // const TABLE_COLUMNS = [SELECT id, title, published, article, author FROM blog_post WHERE CONCAT(title,article, author) LIKE "%alan%"]
 
 async function getBlogPosts() {
-    const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post`)
-    return results
+    try {
+        const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post`)
+        return results
+    } catch (error) {
+        return []
+    }
 }
 
 async function searchByTitle(title) {
-    const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE title LIKE "%${title}%"`)
-    return results
+    try {
+        const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE title LIKE "%${title}%"`)
+        return results
+    } catch (error) {
+        return []
+    }
 }
 
 async function searchByAuthor(author) {
-    const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE author LIKE "%${author}%"`)
-    return results
+    try {
+        const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE author LIKE "%${author}%"`)
+        return results
+    } catch (error) {
+        return []
+    }
 }
 
 async function searchByArticle(article) {
-    const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE article LIKE "%${article}%"`)
-    return results
+    try {
+        const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE article LIKE "%${article}%"`)
+        return results
+    } catch (error) {
+        return []
+    }
 }
 
 async function getById(id) {
-    const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE id=${id}`)
-    return results
+    try {
+        const results = await db.query(`SELECT ${TABLE_COLUMNS.join(',')} FROM blog_post WHERE id=${id}`)
+        return results[0]
+    } catch (error) {
+        return {}
+    }
 }
 
 async function searchAllContent(searchValue) {
-    let myQuery = `SELECT {0} FROM blog_post WHERE CONCAT({1}) LIKE "%${searchValue}%"`
-    myQuery = myQuery.replace('{0}', TABLE_COLUMNS.join(','))
-    myQuery = myQuery.replace('{1}', TABLE_COLUMNS.slice(1, 5).join(`,`))
-    const results = await db.query(myQuery)
-    return results
+    try {
+
+        let myQuery = `SELECT {0} FROM blog_post WHERE CONCAT({1}) LIKE "%${searchValue}%"`
+        myQuery = myQuery.replace('{0}', TABLE_COLUMNS.join(','))
+        myQuery = myQuery.replace('{1}', TABLE_COLUMNS.slice(1, 5).join(`,`))
+        const results = await db.query(myQuery)
+        return results
+    } catch (error) {
+        return []
+    }
 }
 
 async function createBlogPost(blogpost) {
-    let myQuery = "CALL create_blog_post(?,?,?,@result)";
-    const results = await db.query(myQuery, [blogpost.title, blogpost.article, blogpost.author], (error, result) => {
-        if (error) {
-            console.log("Error:", error);
-        } else {
-            console.log("Nuevo ID post:", result);
-        }
-    })
-    return results
+    try {
+        let myQuery = "CALL create_blog_post(?,?,?,@result)";
+        const results = await db.query(myQuery, [blogpost.title, blogpost.article, blogpost.author], (error, result) => {
+            if (error) {
+                console.log("Error:", error);
+            } else {
+                console.log("Nuevo ID post:", result);
+            }
+        })
+        return results
+    } catch (error) {
+        return []
+    }
 }
 
 module.exports = {
