d0785465bb30e39ca978d06df4c775ba5be916c7
Alan Jimenez <alanjq@outlook.com>
2024-02-28 13:24:37 -0500

    Add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+import Blog from './BlogLayout.jsx';
+import './index.css';
+import BlogLayout from './BlogLayout.jsx';
+import BlogPage from './pages/BlogPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import NewPostPage from './pages/NewPostPage.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BlogLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="new-post" element={<NewPostPage />} />
+          <Route path=":postId" element={<BlogPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
