b899cd2880f15285008b406a10dfb8e399ebfc74
Alan Jimenez <alanjq@outlook.com>
2024-02-28 16:35:06 -0500

    Adjust props for className

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,12 +1,12 @@
 export default function TextField(props) {
-    const inputClasses = "flex border w-full px-2 outline-orange-300 py-1 border-cyan-600"
+    const inputClasses = `flex border w-full px-2 outline-orange-300 py-1 border-cyan-600 ${props.className || ''}`
 
     return <fieldset className="col-auto flex flex-col gap-1 my-2">
         <label className="font-bold flex text-slate-600">{props.label}</label>
         {props?.type == 'area' ?
-            <textarea type="text" rows="5" className={inputClasses} {...props} ></textarea>
+            <textarea type="text" rows="5" {...props} className={inputClasses}></textarea>
             :
-            <input type="text" className={inputClasses} {...props} />
+            <input type="text" {...props} className={inputClasses} />
         }
     </fieldset>
 }
